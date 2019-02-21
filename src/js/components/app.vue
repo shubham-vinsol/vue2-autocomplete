bc7724ce12dbd3731e8e2ffcae6c5bc0b6ceeeed
GashVinsol <gaurav@vinsol.com>
2019-02-22 02:38:24 +0530

    scrollFix

---
+++ src/js/components/app.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div>
+    <autocomplete url="https://maps.googleapis.com/maps/api/geocode/json?address="
+                  anchor="formatted_address"
+                  label="geometry.location.lat"
+                  name="autocomplete"
+                  maxHeight="150px"
+                  :customParams="{ token: 'dev' }"
+                  :classes="{ input: 'form-control', wrapper: 'input-wrapper', item: 'list-item'}"
+                  :process="processJSON"
+                  :onSelect="handleSelect">
+    </autocomplete>
+
+    <pre v-if="preContent"
+         :style="preStyle">
+      <b>Selected Data:</b>
+      {{ preContent }}
+    </pre>
+  </div>
+</template>
+
+<script>
+import Autocomplete from "./vue-autocomplete.vue";
+require("../../../dist/style/vue2-autocomplete.css");
+
+export default {
+  components: { Autocomplete },
+
+  data() {
+    return {
+      preContent: "",
+      preStyle: {
+        background: "#f2f2f2",
+        fontFamily: "monospace",
+        fontSize: "1em",
+        display: "inline-block",
+        padding: "15px 7px"
+      }
+    };
+  },
+
+  methods: {
+    processJSON(json) {
+      return json.results;
+    },
+
+    handleSelect(data) {
+      this.preContent = JSON.stringify(data, null, 4);
+    }
+  }
+};
+</script>
---
+++ src/js/plugin.js
@@ -0,0 +1,2 @@
+import plugin from './components/vue-autocomplete.vue'
+export default plugin;
